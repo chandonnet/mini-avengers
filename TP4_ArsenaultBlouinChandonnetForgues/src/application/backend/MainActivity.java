5fd410bbb91f5a958ffd855f56fced19d3a83bdc
Francis <Francis@Francis-HP>
2013-11-26 11:46:15 -0500

    Huge commit (feat. PatArse)

--- TP4_ArsenaultBlouinChandonnetForgues/src/application/backend/MainActivity.java
+++ TP4_ArsenaultBlouinChandonnetForgues/src/application/backend/MainActivity.java
@@ -1,23 +1,56 @@
 package application.backend;
 
+import java.net.*;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+public class MainActivity extends Activity 
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{	
+		Socket s = null;
+		ServerSocket serverSocket = null;
+		
+		
+		
+    	try {
+    		serverSocket = new ServerSocket(1234);
+			s = serverSocket.accept();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button buttonSend = (Button) findViewById(R.id.buttonSend);
+		
+	    buttonSend.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView tV = (TextView) findViewById(R.id.textViewWithScroll);
+				EditText eT = (EditText) findViewById(R.id.editTextChat);
+				CharSequence text = tV.getText() + "\n"+"Pat:"+ eT.getText();
+				eT.setText("");
+				tV.setText(text);
+			}
+		}); 
+	    
+	    Button buttonExit = (Button) findViewById(R.id.buttonExit);
+		
+	    buttonExit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				 finish();
+		         System.exit(0);
+			}
+		}); 
+	}
+	
 }
